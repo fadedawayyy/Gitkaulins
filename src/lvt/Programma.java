71f9057f0d52138d5c74f2184bad16fdf8f1bb2d
Gotins <germans.gotins@sk.lvt.lv>
2025-09-02 12:02:28 +0300

    Izveidota metode kaulina mesanai!

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,12 +3,14 @@ package lvt;
 import java.util.Random;
 
 public class Programma {
-
-	public static void main(String[] args) {
+	static void mestKaulinu () {
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
+	public static void main(String[] args) {
+		mestKaulinu();
 		
 	}
 
