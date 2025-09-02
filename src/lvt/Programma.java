d22a4eb97a3a301c3d4aae75880ea747c38c46a1
Gotins <germans.gotins@sk.lvt.lv>
2025-09-02 12:15:42 +0300

    Ieviesta iespeja ievadit metienu skaitu

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -1,19 +1,22 @@
 package lvt;
 
 import java.util.Random;
-
+import java.util.Scanner;
 public class Programma {
-	static void mestKaulinu (int reizes) {
-		int skaitlis;
+	
+
+	public static void main(String[] args) {
+		int skaitlis, reizes;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu?");
+		reizes = scan.nextInt();
 		for(int i=1; i<=reizes;i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-		}
-	}
-	public static void main(String[] args) {
-		mestKaulinu(5);
 		
 	}
+		scan.close();
 
+}
 }
